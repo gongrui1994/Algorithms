002d72e33ecd3382eea2b41d4611bcf98d527181
gongrui <gongrui1994@163.com>
2017-06-18 16:47:16 +0800

    添加算法导论快速排序算法

---
+++ src/main/java/sort/advance/QuickSort.java
@@ -0,0 +1,43 @@
+package sort.advance;
+
+/**
+ * Created by gongrui on 2017/6/18.
+ */
+public class QuickSort {
+
+    public static void swap(int[] a,int i,int j) {
+        int temp=a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    //算法导论
+    public static int partition(int[] a,int p,int r) {
+        int x = a[r];//比较基准
+        int i=p-1;
+        for(int j=p;j<r;j++) {
+            if(a[j] <= x) {
+                i++;
+                swap(a,i,j);
+            }
+        }
+        swap(a,i+1,r);
+        return i+1;
+    }
+
+    public static void quickSort(int[] a,int p,int r) {
+        if(p<r) {
+            int q = partition(a,p,r);
+            quickSort(a,p,q-1);
+            quickSort(a,q+1,r);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {2,8,7,1,3,5,6,4};
+        quickSort(a,0,a.length-1);
+        for(int e:a) {
+            System.out.println(e);
+        }
+    }
+}
