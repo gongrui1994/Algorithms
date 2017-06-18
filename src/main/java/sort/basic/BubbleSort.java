b65a9933cd6988eda705400777c08ff15d482f57
gongrui <gongrui1994@163.com>
2017-06-18 16:46:39 +0800

    修改冒泡排序算法为从小到大排序

--- src/main/java/sort/basic/BubbleSort.java
+++ src/main/java/sort/basic/BubbleSort.java
@@ -15,7 +15,7 @@ public class BubbleSort {
         int n=a.length;
         for(int i=0;i<n;i++) {
             for(int j=n-1;j>i;j--) {
-                if(a[j] > a[j-1]) {
+                if(a[j] < a[j-1]) {
                     swap(a,j,j-1);
                 }
             }
