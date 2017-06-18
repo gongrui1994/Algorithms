03d5d6130af417a32b2fd7a8addceccf49747ae8
gongrui <gongrui1994@163.com>
2017-06-18 10:40:56 +0800

    新增选择排序

---
+++ src/main/java/sort/SelectionSort.java
@@ -0,0 +1,38 @@
+package sort;
+
+/**
+ * Created by gongrui on 2017/6/18.
+ */
+public class SelectionSort {
+
+    public static void swap(int[] a,int i,int j) {
+        int temp=a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+
+    public static void sort(int[] a) {
+        int n=a.length;
+        int lowIndex;
+        int lowValue;
+        for(int i=0;i<n;i++) {
+            lowIndex=i;
+            lowValue=a[i];
+            for(int j=i;j<n;j++) {
+                if(a[j] < lowValue) {
+                    lowIndex = j;
+                    lowValue = a[j];
+                }
+            }
+            swap(a,i,lowIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {5, 2, 4, 6, 1, 3};
+        sort(a);
+        for(int e:a) {
+            System.out.println(e);
+        }
+    }
+}
