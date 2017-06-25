06b24bd9b09f77488e6c5c3b3e2ab84aff13b20a
gongrui <gongrui1994@163.com>
2017-06-25 13:54:25 +0800

    更新计数排序算法

--- src/main/java/sort/linear/CountingSort.java
+++ src/main/java/sort/linear/CountingSort.java
@@ -6,8 +6,6 @@ package sort.linear;
  */
 public class CountingSort {
 
-    public static final int INFINITY=50000;
-
     /**
      *
      * @param a
@@ -15,7 +13,7 @@ public class CountingSort {
      * @param k 最小为a数组中最大数+1，c[i]表示i在a数组中出现的次数
      */
     public static void countingSort(int[] a, int[] b, int k) {
-        int[] c = new int[k];
+        int[] c = new int[k+1];
         for(int i=0;i<c.length;i++) {
             c[i] = 0;
         }
@@ -23,7 +21,7 @@ public class CountingSort {
             c[a[i]]++;
         }
         //till here，c[i]代表i出现的次数
-        for(int i=1;i<k;i++) {
+        for(int i=1;i<c.length;i++) {
             c[i] = c[i]+c[i-1];
         }
         //till here，c[i]表示小于或等于i的数的个数
@@ -36,7 +34,7 @@ public class CountingSort {
     public static void main(String[] args) {
         int[] a = {2,5,3,0,2,2,0,3};
         int[] b = new int[a.length];
-        countingSort(a,b,INFINITY);//+1的目的是增加数组长度 使之能存放0-8的出现次数
+        countingSort(a,b,5);//+1的目的是增加数组长度 使之能存放0-8的出现次数
         for(int e:b) {
             System.out.println(e);
         }
