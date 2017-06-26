9cab8881b1d576a2e2ec9bcddebb620bd571b4ea
gongrui <gongrui1994@163.com>
2017-06-26 09:54:10 +0800

    新增朴素字符串匹配算法

---
+++ src/main/java/string/NaiveStringMatcher.java
@@ -0,0 +1,29 @@
+package string;
+
+/**
+ * 朴素字符串匹配算法
+ * Created by gongrui on 2017/6/26.
+ */
+public class NaiveStringMatcher {
+
+    /**
+     * 朴素匹配
+     * 一共有n-m+1种可能
+     * @param t 文本
+     * @param p 模式
+     * @return
+     */
+    public static void matcher(String t, String p) {
+        for(int i=0;i<t.length()-p.length()+1;i++) {
+            if(p.equals(t.substring(i,i+p.length()))) {
+                System.out.println("偏移s="+i);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String t = "acaabcaabac";
+        String p = "aab";
+        matcher(t, p);
+    }
+}
